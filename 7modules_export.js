8e28d8159c532c9bedea564744aa27cb51e763ff
subratapal0199 <subratapal0199@gmail.com>
2022-12-27 23:36:23 +0530

    some code for testing purpose

--- 7modules_export.js
+++ 7modules_export.js
@@ -3,3 +3,28 @@ JavaScript modules allow you to break up your code into separate files.This make
 JavaScript modules rely on the import and export statements.
 You can export a function or variable from any file.
 */
+//in-line individually
+console.log("Hello from 7modules_export.js");
+
+const getCircleArea=r=>3.1416*r*r;
+//now we can export it into another file
+console.log(getCircleArea(4));
+
+const PI=3.1416;
+
+module.exports.getCircleArea=getCircleArea;
+module.exports.PI=PI;
+
+//Another way we can exports 
+
+module.exports.getcirarea=r=>3.1416*r*r;
+module.exports.Pi=3.1416;
+
+console.log(module.exports);
+
+module.exports.message=() => {
+    const name = "Subrata";
+    const age = 23;
+    //return name + ' is ' + age + 'years old.';
+    };
+
--- 8modules_import.js
+++ 8modules_import.js
@@ -1,2 +1,27 @@
 /*You can import modules into a file in two ways, based on if they are named exports or default exports.
 Named exports are constructed using curly braces. Default exports are not.*/
+const circle=require('./7modules_export');
+console.log(circle);
+
+const a=circle.getCircleArea(24);
+console.log(a);
+
+//another way we can import
+const {getcirarea,Pi,}=require('./7modules_export');
+console.log(getcirarea(5));
+
+
+//Asign a object into another object
+const obj={
+    name1:'Subrata',
+    age:24,
+    city:'Kolkata'
+}
+//aisgn into another object
+const newobj=obj; // or we can asign another way
+const {age,name1,city}=obj;  // destructturing
+
+//export file 
+const {message}=require('./7modules_export');
+console.log(message.name); 
+
