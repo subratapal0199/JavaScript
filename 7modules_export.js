f624a9d886f0e8bb79277e07c8014050bb73f6c0
subratapal0199 <subratapal0199@gmail.com>
2022-12-27 20:16:33 +0530

    JavaScript import and export. We can import file form same dir

---
+++ 7modules_export.js
@@ -0,0 +1,5 @@
+/*
+JavaScript modules allow you to break up your code into separate files.This makes it easier to maintain the code-base.
+JavaScript modules rely on the import and export statements.
+You can export a function or variable from any file.
+*/
---
+++ 8modules_import.js
@@ -0,0 +1,2 @@
+/*You can import modules into a file in two ways, based on if they are named exports or default exports.
+Named exports are constructed using curly braces. Default exports are not.*/
